47f75f9f7af5c13afe03c926b944d431ae51f9fb
dllo <329798528@qq.com>
2018-01-26 17:59:05 +0800

    提交ssm框架中的mybatis基础操作 1.分页查询 2.模糊查询

---
+++ src/main/java/com/ssm/test/MainTest.java
@@ -0,0 +1,54 @@
+package com.ssm.test;
+
+import com.ssm.domain.Student;
+import com.ssm.mapper.StudentMapper;
+import com.ssm.page.PageBean;
+import org.junit.Before;
+import org.junit.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import java.util.List;
+
+/**
+ * Created by dllo on 18/1/26.
+ */
+public class MainTest {
+
+    private ApplicationContext context;
+
+    @Before
+    public void init(){
+        context = new ClassPathXmlApplicationContext("classpath*:spring-*.xml");
+    }
+//    @Test
+//    public void testMapper(){
+//        //获得mybatis的Student表的代理对象
+//        StudentMapper studentMapper = context.getBean(StudentMapper.class);
+//        //调用接口中的查询所有
+//        List<Student> students = studentMapper.selectAll();
+//        for (Student student : students) {
+//            System.out.println(student);
+//        }
+//        int count = studentMapper.getTotalRecord();
+//        System.out.println(count);
+//
+//        PageBean<Student> pageBean = new PageBean<Student>(1,3,count);
+//        System.out.println(studentMapper.pageSelect(pageBean));
+//    }
+
+//    @Test
+//    public void testPageSelect(){
+//        StudentMapper studentMapper = context.getBean(StudentMapper.class);
+//
+//        int total = studentMapper.getTotalRecord();
+//        PageBean<Student> pageBean = new PageBean<Student>(1,3,total);
+//        Student param = new Student();
+//        param.setSname("孙");
+//        pageBean.setParameter(param);
+//        List<Student> students = studentMapper.pageSelect(pageBean);
+//        System.out.println(students);
+//    }
+
+
+}
